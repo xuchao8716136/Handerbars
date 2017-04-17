d069a12ad3a59a2b3c49825dce7c08b7eea88c29
xuchao <xuchao8716136@g,ail.com>
2017-04-17 15:21:15 +0800

    ss

--- server.js
+++ server.js
@@ -3,11 +3,23 @@ var url = require("url");
 
 function start(route, handle) {
     function onRequest(request, response) {
+        // 将数据给分段缓存
+        var postData = '';
         var pathname = url.parse(request.url).pathname;
-        console.log("Request for " + pathname + " received.");
-        route(handle, pathname, response, request);
-    }
+        // 请求是客户端发送的，客户端发送的请求在 request里面
+        // 然后服务器接受了客户端发送的数据，每次访问时，就是一次请求的送达，返回的信息用 response 来处理
 
+        console.log("Request for " + pathname + " received.");
+        request.setEncoding('utf-8')
+        request.addListener('data',function (postDataChunk) {
+            postData += postDataChunk
+
+        })
+        request.addListener('end',function () {
+            route(handle, pathname, response, postData)
+        })
+        route(handle, pathname, response)
+    }
     http.createServer(onRequest).listen(8888);
     console.log("Server has started.");
 }
